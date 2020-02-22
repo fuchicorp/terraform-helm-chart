a77108a459d47b1f75f4904cc57ffacae009e0f3
Farkhod Sadykov <sadykovfarkhod@gmail.com>
2020-02-21 20:06:25 -0600

    Added required provider versions

---
+++ versions.tf
@@ -0,0 +1,16 @@
+
+# required helm provider for this module 
+provider "helm" { 
+    version = "0.10.4"
+}
+
+# Required provider for local files 
+provider "local" {
+    version = "1.4.0"
+}
+
+# We are using 2.1.2 for helm deplpoy 
+provider "template" {
+    version  = "2.1.2"
+}
+
